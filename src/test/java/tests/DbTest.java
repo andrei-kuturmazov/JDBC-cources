ae9e385dc61591bd43d4880900e9ec46ae90d96f
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-07-31 12:49:05 +0300

    select request adding

--- src/test/java/tests/DbTest.java
+++ src/test/java/tests/DbTest.java
@@ -3,6 +3,9 @@ package tests;
 import dbConnection.JDBCConnection;
 import org.junit.jupiter.api.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayName("Проверка подключения и отправки запросов к БД users")
@@ -19,11 +22,35 @@ public class DbTest extends TestInit {
     @Order(2)
     @DisplayName("Создание таблицы students в БД")
     void testTableCreation() {
-        String query = "CREATE TABLE if not exists students ("
-                +    "Id INT (5) NOT NULL PRIMARY KEY AUTO_INCREMENT,"
-                +    "age INT,"
-                +    "FirstName varchar(20) NOT NULL,"
-                +    "LastName varchar(20) NOT NULL)";
+        String query = "CREATE TABLE if not exists students\n" +
+                "    (Id INT (5) NOT NULL PRIMARY KEY AUTO_INCREMENT,\n" +
+                "    FirstName varchar(20) NOT NULL,\n" +
+                "    LastName varchar(20) NOT NULL,\n" +
+                "    HomeTown varchar(20),\n" +
+                "    Age INT);";
         JDBCConnection.createTable(query);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Проверка добавления данных в таблицу students")
+    void testDataAdding() {
+        String query = "Insert students(FirstName, LastName, HomeTown, Age) VALUES\n" +
+                "('Andrei', 'Kuturmazov', 'Grodno', 31),\n" +
+                "('Stepan', 'Petrov', 'Minsk', 33),\n" +
+                "('Hanna', 'Kulba', 'Grodno', 18),\n" +
+                "('Karyna', 'Karmaza', 'Rymdyni', 18);";
+        JDBCConnection.insertDataToDB(query);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Проверка получения информации о студенте")
+    void testFirstUserData() throws SQLException {
+        String query = "SELECT FirstName, LastName from students where Id = 1;";
+        ResultSet resultSet = JDBCConnection.selectDataFromDB(query);
+        resultSet.first();
+        Assertions.assertEquals("Andrei", resultSet.getString("FirstName"));
+        Assertions.assertEquals("Kuturmazov", resultSet.getString("LastName"));
     }
 }
