9fcf3e708e7899e4e5ac8f683c9d8f53b87200d1
andrei.kuturmazov <andrei.kuturmazov@gmail.com>
2020-07-31 11:44:21 +0300

    adding UTF-8 properties to gradle

--- src/test/java/tests/DbTest.java
+++ src/test/java/tests/DbTest.java
@@ -5,13 +5,25 @@ import org.junit.jupiter.api.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@DisplayName("Проверка подключения к БД users и отправки запросов")
+@DisplayName("Проверка подключения и отправки запросов к БД users")
 public class DbTest extends TestInit {
 
     @Test
     @Order(1)
     @DisplayName("Test DB connection")
-    public void testDbConnection() {
+    void testDbConnection() {
         Assertions.assertNotNull(JDBCConnection.connectToDb());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Создание таблицы students в БД")
+    void testTableCreation() {
+        String query = "CREATE TABLE if not exists students ("
+                +    "Id INT (5) NOT NULL PRIMARY KEY AUTO_INCREMENT,"
+                +    "age INT,"
+                +    "FirstName varchar(20) NOT NULL,"
+                +    "LastName varchar(20) NOT NULL)";
+        JDBCConnection.createTable(query);
     }
 }
